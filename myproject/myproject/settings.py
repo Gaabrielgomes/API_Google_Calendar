1edb31372af1be3cb7da25fe881814c2757a7b39
Gabriel <gaabrielgomes3241@gmail.com>
2024-09-11 11:38:30 -0300

    Configurações de provedor de autorização, token e url feitos

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'calendar_integration',
+    'oauth2_provider',
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'myproject.urls'
@@ -122,3 +126,15 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+OAUTH2_PROVIDER = {
+    'SCOPES': {'calendar': 'Read/write access to Calendar'},
+    'CLIENT_ID': 'YOUR CLIENT ID',
+    'CLIENT_SECRET': 'YOUR CLIENT_SECRET',
+}
+
+GOOGLE_TOKEN_FILE = 'token.json'
+
+CORS_ALLOW_ORIGINS = [
+    'http://localhost:8000/'
+]
