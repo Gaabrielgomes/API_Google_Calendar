6584f4cc88ef7441846baa09f44558f54145df2b
Gabriel <gaabrielgomes3241@gmail.com>
2024-09-11 12:00:17 -0300

    Criado o modelo de evento e a forma de adicionar um evento pelo admin

--- myproject/calendar_integration/models.py
+++ myproject/calendar_integration/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Event(models.Model):
+    summary = models.CharField(max_length=255, default='')
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+
+    def __str__(self):
+        return f'Summary: {self.summary} | Start time: {self.start_time} | End time: {self.end_time}'
+ 
